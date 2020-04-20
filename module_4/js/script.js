99a5b33d23f2ed9087704f7e15be980aa4a93d3a
Kartik153 <55834515+Kartik153@users.noreply.github.com>
2020-04-20 18:11:03 +0530

    Update script.js

--- module_4/js/script.js
+++ module_4/js/script.js
@@ -1,11 +1,11 @@
 (function() {
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-	for (i=0;i<names.length;i++){
-		var firstLetter = names[i][0].toLowerCase();
-		if (firstLetter === 'j') {
-			byeSaid.said(names[i]);
-		} else {
-			helloSaid.said(names[i]);
-		}
-	}
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (i=0;i<names.length;i++){
+    var firstLetter = names[i][0].toLowerCase();
+    if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
